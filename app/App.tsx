5097feaa2c626182de2146cc2df7b025f2dee8d3
Elsa Polo Laube <elsa.polo.laube@gmail.com>
2024-01-18 12:46:52 +0000

    set up linter

--- app/App.tsx
+++ app/App.tsx
@@ -1,13 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export default function App() {
+export const App: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>Welcome to kayak app</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +16,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
