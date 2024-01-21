6fb973a8c6f1bd618c1443a3b4547de58dc0f428
jamielewisturner <jamiehattie@gmail.com>
2024-01-21 15:34:31 +0000

    Add / route, (to test deployment)

--- backend/src/main/kotlin/com/kayak_backend/routes/TestRoutes.kt
+++ backend/src/main/kotlin/com/kayak_backend/routes/TestRoutes.kt
@@ -5,6 +5,11 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.testRouting() {
+    route("/") {
+        get {
+            call.respondText("Hello, this is a kayak app")
+        }
+    }
     route("/test") {
         get {
             call.respondText("Hello, this is a kayak app")
