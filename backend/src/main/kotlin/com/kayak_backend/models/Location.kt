4daab42036fdbd78d81094976edf24cb9c9386d7
Elsa Polo Laube <elsa.polo.laube@gmail.com>
2024-01-25 17:26:04 +0000

    made a Location model with lat lon and a distance calculator

---
+++ backend/src/main/kotlin/com/kayak_backend/models/Location.kt
@@ -0,0 +1,24 @@
+package com.kayak_backend.models
+
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
+
+data class Location(val latitude: Double, val longitude: Double){
+
+    private val RADIUS: Double = 6371.0;
+    fun distance (coord2 : Location) : Double{
+        val coord1 = this;
+        val deltaLat = Math.toRadians(coord2.latitude - coord1.latitude)
+        val deltaLon = Math.toRadians(coord2.longitude - coord1.longitude)
+
+        val a = sin(deltaLat / 2) * sin(deltaLat / 2) +
+                cos(Math.toRadians(coord1.latitude)) * cos(Math.toRadians(coord2.latitude)) *
+                sin(deltaLon / 2) * sin(deltaLon / 2)
+
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+        return RADIUS * c
+    }
+}
