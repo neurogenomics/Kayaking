ecb8368dc642203cc5dadae4ef45fd4b49028d1a
Joel Ulens <joelulens@dyn3176-104.wlan.ic.ac.uk>
2024-01-26 15:59:45 +0000

    created wind service package

---
+++ backend/src/main/kotlin/com/kayak_backend/services/wind/WindService.kt
@@ -0,0 +1,9 @@
+package com.kayak_backend.services.wind
+
+import com.kayak_backend.models.Location
+import com.kayak_backend.models.WindInfo
+import java.time.LocalDateTime
+
+interface WindService {
+    fun getWind(loc: Location, time: LocalDateTime): WindInfo
+}
