7e780557914c2f49f825396aa5965f3c3759bbdd
Elsa Polo Laube <elsa.polo.laube@gmail.com>
2024-01-26 15:39:35 +0000

    fixed linting

--- app/src/services/slipwayService.ts
+++ app/src/services/slipwayService.ts
@@ -1,11 +1,10 @@
 import { LocationModel } from '../models/locationModel';
 import { getData } from './utils';
 
-
 export const getClosestSlipway = async (
   location: LocationModel,
 ): Promise<LocationModel> => {
-  let url = `slipway?lat=${location.latitude}&lng=${location.longitude}`;
+  const url = `slipway?lat=${location.latitude}&lng=${location.longitude}`;
   const closestSlipway = await getData<LocationModel>(url);
   return closestSlipway;
 };
--- app/tests/services/slipwayService.test.ts
+++ app/tests/services/slipwayService.test.ts
@@ -20,11 +20,16 @@ describe('getClosestSlipway', () => {
   it('should call getData with the correct URL', async () => {
     await getClosestSlipway(location);
 
-    expect(getDataMock).toHaveBeenCalledWith('slipway?lat=1.0721198&lng=50.688823');
+    expect(getDataMock).toHaveBeenCalledWith(
+      'slipway?lat=1.0721198&lng=50.688823',
+    );
   });
 
   it('should return the location from getData', async () => {
-    const mockSunsetInfo: LocationModel = {latitude: 1.0721198, longitude: 50.688823};
+    const mockSunsetInfo: LocationModel = {
+      latitude: 1.0721198,
+      longitude: 50.688823,
+    };
 
     getDataMock.mockResolvedValue(mockSunsetInfo);
     const result = await getClosestSlipway(location);
