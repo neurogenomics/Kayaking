66415d7d4ca855a29461e59fa79be38d3295fe12
3mily8rown <69977813+3mily8rown@users.noreply.github.com>
2024-02-03 13:53:16 +0000

    corrected latitude and longitude in windFiltering

--- backend/src/main/kotlin/com/kayak_backend/services/routeFiltering/WindFiltering.kt
+++ backend/src/main/kotlin/com/kayak_backend/services/routeFiltering/WindFiltering.kt
@@ -1,30 +1,35 @@
 package com.kayak_backend.services.routeFiltering
 
+import com.kayak_backend.getConf
+import com.kayak_backend.getWindService
 import com.kayak_backend.models.Location
-import com.kayak_backend.services.wind.GribWindFetcher
+import com.kayak_backend.services.wind.WindService
 import java.io.File
 import java.time.LocalDateTime
 
 class WindFiltering {
 
     // if file does not exist, run the main function in DirectionToSea.kt
+    // need to decide if we store in the csv or if we run the directionToSea class each time
+    // could it be done in a setup stage?
     private val seaBearingPath = "src/main/resources/seaBearings.csv"
 
     fun classifyAreas(){
-        val windService = GribWindFetcher()
+        val windService : WindService = getWindService(getConf("./config.yaml"))
         File(seaBearingPath).useLines { lines ->
             val dataLines = lines.drop(1)
 
             dataLines.forEach { line ->
                 val point = line.split(",")
-                println(point[0]) //bearing
-                println(point[1]) //y lon
-                println(point[2]) //x lat
-                val lat = -1.29
-                val lon = 50.76
-                // val wind = windService.getWind(Location((point[2]).toDouble(), (point[1]).toDouble()), LocalDateTime.now())
+                val bearing = (point[0]).toDouble()
+                val lat = (point[2]).toDouble()
+                val lon = (point[1]).toDouble()
+
+                //time needs to be within range of the grib file
                 val wind = windService.getWind(Location(lat, lon), LocalDateTime.now())
                 println("${wind.u} and ${wind.v}")
+
+                //TODO find resultant wind direction and if <90 different to bearing out to sea, mark as bad (or filter)
             }
         }
 
