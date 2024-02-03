8e5e501fa40e679f6c10d90643bcef6be6409f0b
3mily8rown <69977813+3mily8rown@users.noreply.github.com>
2024-02-03 23:56:53 +0000

    added TODO comments to windFiltering

--- backend/src/main/kotlin/com/kayak_backend/services/routeFiltering/WindFiltering.kt
+++ backend/src/main/kotlin/com/kayak_backend/services/routeFiltering/WindFiltering.kt
@@ -6,6 +6,7 @@ import com.kayak_backend.models.Location
 import com.kayak_backend.services.wind.WindService
 import java.io.File
 import java.time.LocalDateTime
+import kotlin.math.abs
 
 class WindFiltering {
 
@@ -22,14 +23,22 @@ class WindFiltering {
             dataLines.forEach { line ->
                 val point = line.split(",")
                 val bearing = (point[0]).toDouble()
-                val lat = (point[2]).toDouble()
-                val lon = (point[1]).toDouble()
+                val lon = (point[2]).toDouble()
+                val lat = (point[1]).toDouble()
 
                 //time needs to be within range of the grib file
-                val wind = windService.getWind(Location(lat, lon), LocalDateTime.now())
+                //jan25 12pm until jan27 12pm
+                val wind = windService.getWind(Location(lat, lon), LocalDateTime.of(2024, 1, 25, 12, 1))
+                // val wind = windService.getWind(Location(lat, lon), LocalDateTime.now())
                 println("${wind.u} and ${wind.v}")
+                val result = 0
 
                 //TODO find resultant wind direction and if <90 different to bearing out to sea, mark as bad (or filter)
+                if (abs(result - bearing) < 90){
+                    //mark as bad
+                    // either add to csv or add indicator to list depending how we decided
+                    // or maybe accumulate 10 then decide if that group is good/bad
+                }
             }
         }
 
