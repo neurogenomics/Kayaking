b7c12d733722091aa6240f295a51872cb79ce32a
Joel Ulens <joelulens@Joels-MacBook-Pro-2.local>
2024-02-06 22:07:31 +0000

    linting 2

--- backend/src/main/kotlin/com/kayak_backend/interpolator/Interpolator.kt
+++ backend/src/main/kotlin/com/kayak_backend/interpolator/Interpolator.kt
@@ -3,8 +3,10 @@ package com.kayak_backend.interpolator
 interface Interpolator {
     fun interpolate(
         data: Array<Array<Double>>,
-        indices: Pair<Array<Double>, Array<Double>>, // Length should be equal to the lengths of the array
-        ranges: Pair<Pair<Double, Double>, Pair<Double, Double>>, // ranges must be in ascending order
+        indices: Pair<Array<Double>, Array<Double>>,
+        // Length should be equal to the lengths of the array
+        ranges: Pair<Pair<Double, Double>, Pair<Double, Double>>,
+        // ranges must be in ascending order
         resolutions: Pair<Double, Double>,
     ): Triple<Array<Array<Double>>, List<Double>, List<Double>>
 }
