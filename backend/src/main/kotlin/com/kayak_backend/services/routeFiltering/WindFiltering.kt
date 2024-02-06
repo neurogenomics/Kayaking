0ad1b8c967050b5a5aa312bb160078295cf19613
3mily8rown <69977813+3mily8rown@users.noreply.github.com>
2024-02-06 17:52:31 +0000

    implemented function to find bad wind areas

--- backend/src/main/kotlin/com/kayak_backend/services/routeFiltering/WindFiltering.kt
+++ backend/src/main/kotlin/com/kayak_backend/services/routeFiltering/WindFiltering.kt
@@ -2,15 +2,16 @@ package com.kayak_backend.services.routeFiltering
 
 import com.kayak_backend.getConf
 import com.kayak_backend.getWindService
-import com.kayak_backend.models.Location
+import com.kayak_backend.models.WindInfo
 import com.kayak_backend.services.seaBearing.SeaBearingService
-import com.kayak_backend.services.seaBearing.SeaBearingsGetter
 import com.kayak_backend.services.wind.WindService
-import java.io.File
 import java.time.LocalDateTime
 import kotlin.math.abs
+import kotlin.math.atan2
 
+private const val BAD_WIND_LIMIT = 90
 class WindFiltering {
+
 
     fun classifyAreas(){
         val windService : WindService = getWindService(getConf("./config.yaml"))
@@ -18,20 +19,27 @@ class WindFiltering {
 
         seaBearings.forEach{
             val wind = windService.getWind(it.coor, LocalDateTime.of(2024,1,25,12,1))
-            println("${wind.u} and ${wind.v}")
-            val result = 0
 
-            //TODO find resultant wind direction and if <90 different to bearing out to sea, mark as bad (or filter)
-            if (badArea(it.bearing, result)){
+            if (badArea(it.bearing, wind)){
                 //mark as bad
-                // either add to csv or add indicator to list depending how we decided
                 // or maybe accumulate 10 then decide if that group is good/bad
+                println("bad :(")
+            } else {
+                println("good :)")
             }
         }
     }
 
-    private fun badArea(bearing: Double, result: Int): Boolean {
-        return false
+    private fun badArea(bearing: Double, wind: WindInfo): Boolean {
+        val displace = if (wind.v < 0) 180.0 else 0.0
+        val resultWind = Math.toDegrees(atan2(wind.u, wind.v)) + displace
+        val dif = abs(resultWind - bearing)
+
+        println("Bearing is $bearing")
+        println("Wind is ${wind.u},${wind.v}")
+        println(resultWind)
+
+        return (dif < BAD_WIND_LIMIT || dif > (360 - BAD_WIND_LIMIT))
     }
 
 }
