8e01959f360a13a8ac9076abd51a80572910eb65
Elsa Polo Laube <elsa.polo.laube@gmail.com>
2024-02-08 19:45:34 +0000

    linting again

--- backend/src/main/kotlin/com/kayak_backend/services/Consts.kt
+++ backend/src/main/kotlin/com/kayak_backend/services/Consts.kt
@@ -2,7 +2,7 @@ package com.kayak_backend.services
 
 import com.kayak_backend.models.Location
 
-//corners of a rectangle that frames the isle of wight
+// corners of a rectangle that frames the isle of wight
 
 val isleOfWightLocation1 = Location(50.564485309567644, -1.6005677025384493)
 val isleOfWightLocation2 = Location(50.8605772841442, -1.0457581322259493)
