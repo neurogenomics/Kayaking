e954d5de6fd0d564e37ed23cc180f70bb0f0f94a
jlt21 <jlt21@ac.ic.uk>
2024-02-08 19:38:43 +0000

    Clean up code

---
+++ backend/src/main/kotlin/com/kayak_backend/services/coastline/GeoJSONCoastline.kt
@@ -0,0 +1,73 @@
+package com.kayak_backend.services.coastline
+
+import com.kayak_backend.services.route.Route
+import okhttp3.OkHttpClient
+import org.json.JSONObject
+import org.locationtech.jts.geom.*
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+import java.io.PrintWriter
+
+
+class IsleOfWightCoastline(private val client: OkHttpClient = OkHttpClient()) : CoastlineService {
+    override fun getCoastline(): Polygon {
+        val json = File("./src/main/kotlin/com/kayak_backend/gribReader/wight.geojson").readText()
+        return parseCoastline(JSONObject(json))
+    }
+
+    private fun parseCoastline(json: JSONObject): Polygon {
+        val coordinates : MutableList<Coordinate> = mutableListOf()
+
+        val jsonCoordinates = json.getJSONArray("features").getJSONObject(0).getJSONObject("geometry").getJSONArray("coordinates").getJSONArray(0)
+
+
+
+            for (i in 0 until jsonCoordinates.length()) {
+                val jsonCoordinate = jsonCoordinates.getJSONArray(i)
+                coordinates.add(Coordinate(jsonCoordinate.getDouble(1), jsonCoordinate.getDouble(0)))
+            }
+            // Close the polygon by adding the first point to the end
+
+
+        coordinates.add(coordinates.first())
+
+        val geometryFactory = GeometryFactory()
+        return geometryFactory.createPolygon(coordinates.toTypedArray())
+    }
+}
+
+
+fun main() {
+    val test = IsleOfWightCoastline()
+    val coast = test.getCoastline()
+
+    val y =  Route.create(coast, 5.0)//Route.extractLargestPart(b)
+
+    try {
+        PrintWriter(FileWriter(File("/home/jamie/thirdyear/tests/coast/out.csv"))).use { writer ->
+            writer.println("latitude,longitude")
+            //writer.println("Polygon: $polygon")
+            for (i in 0 until y.numPoints) {
+        val p0 = y.coordinates[i]
+                writer.println("${p0.x},${p0.y}")
+    }
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+
+    try {
+        PrintWriter(FileWriter(File("/home/jamie/thirdyear/tests/coast/out2.csv"))).use { writer ->
+            writer.println("latitude,longitude")
+            //writer.println("Polygon: $polygon")
+            for (i in 0 until coast.numPoints) {
+                val p0 = coast.coordinates[i]
+                writer.println("${p0.x},${p0.y}")
+            }
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+
+}
