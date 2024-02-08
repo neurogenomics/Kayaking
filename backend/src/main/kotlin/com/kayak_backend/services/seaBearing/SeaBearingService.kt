6510f1d4dc2af2822832f6fd69ffad4d71d4b088
3mily8rown <69977813+3mily8rown@users.noreply.github.com>
2024-02-08 20:10:19 +0000

    deleted old coastline file

--- backend/src/main/kotlin/com/kayak_backend/services/seaBearing/SeaBearingService.kt
+++ backend/src/main/kotlin/com/kayak_backend/services/seaBearing/SeaBearingService.kt
@@ -2,10 +2,6 @@ package com.kayak_backend.services.seaBearing
 
 import com.kayak_backend.services.coastline.CoastlineService
 import com.kayak_backend.services.coastline.IsleOfWightCoastline
-import java.io.File
-import java.io.FileWriter
-import java.io.IOException
-import java.io.PrintWriter
 
 class SeaBearingService(coastlineService: CoastlineService = IsleOfWightCoastline()) {
     private var seaBearings: List<SeaBearingInfo> = listOf()
@@ -18,20 +14,5 @@ class SeaBearingService(coastlineService: CoastlineService = IsleOfWightCoastlin
         }
 
         return seaBearings
-    }
-}
-
-fun main() {
-    val service = SeaBearingService()
-    val bearings = service.getSeaBearings()
-
-    try {
-        PrintWriter(FileWriter(File("src/main/resources/seaBearings.csv"))).use { writer ->
-            for (i in 0..<bearings.size - 1) {
-                writer.println("${bearings[i].bearing},${bearings[i].coor.latitude},${bearings[i].coor.longitude}")
-            }
-        }
-    } catch (e: IOException) {
-        e.printStackTrace()
     }
 }
