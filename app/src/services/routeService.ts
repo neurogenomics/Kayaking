22d10647ae3bee759cc7dbc0789d3b4ee34788f5
jlt21 <jlt21@ac.ic.uk>
2024-02-09 11:27:03 +0000

    Create route frontend service

---
+++ app/src/services/routeService.ts
@@ -0,0 +1,17 @@
+import { Double } from 'react-native/Libraries/Types/CodegenTypes';
+import { LocationModel } from '../models/locationModel';
+import { Route } from '../models/routeModel';
+import { getData } from './utils';
+import { format } from 'date-fns';
+
+export const getRoute = async (
+  startLocation: LocationModel,
+  durationInMins: Double,
+  date?: Date,
+): Promise<Route[]> => {
+  let url = `slipway?lat=${startLocation.latitude}&lon=${startLocation.longitude}&duration=${durationInMins}`;
+  if (date) {
+    url += `&startDateTime=${format(date, 'yyyy-MM-dd')}`;
+  }
+  return await getData<Route[]>(url);
+};
