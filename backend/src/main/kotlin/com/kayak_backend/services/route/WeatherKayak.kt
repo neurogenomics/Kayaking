a13d160e566f1b3591ff05a135cefa8330e52a08
3mily8rown <69977813+3mily8rown@users.noreply.github.com>
2024-02-09 02:16:44 +0000

    looked further into finding resultant spped

--- backend/src/main/kotlin/com/kayak_backend/services/route/WeatherKayak.kt
+++ backend/src/main/kotlin/com/kayak_backend/services/route/WeatherKayak.kt
@@ -10,10 +10,9 @@ import java.time.LocalDateTime
 import kotlin.math.*
 
 class WeatherKayak(
-    val windService : WindService = getWindService(getConf("./config.yaml")),
-    val tideService :TideService = getTideService(getConf("./config.yaml"))
-    ) : Kayak{
-
+    private val windService: WindService = getWindService(getConf("./config.yaml")),
+    private val tideService: TideService = getTideService(getConf("./config.yaml")),
+) : Kayak {
 
     override fun getSpeed(
         dateTime: LocalDateTime,
@@ -23,50 +22,28 @@ class WeatherKayak(
         val windinfo = windService.getWind(location, dateTime)
         val tideinfo = tideService.getTide(location, dateTime)
         val kayakerSpeed = 3.0
-
         val goalBearingRad = Math.toRadians(bearing)
 
         // elsi ou es tu parti ;(
 
-        val weatherU : Double = windinfo.u + tideinfo.u
-        val weatherV : Double = windinfo.v + tideinfo.v
+        val weatherU: Double = windinfo.u + tideinfo.u
+        val weatherV: Double = windinfo.v + tideinfo.v
 
-        //if sin and cos are wrong then it will be opposite
+        // i think these maths equations are all wrong
+        // need to find out how to solve the simultaneous equations :(
         var resultSpeed = 0.0
         var resultBearing = 0.0
-        if (weatherU == 0.0){
-            // do something
-        } else if (sin(goalBearingRad) == 0.0){
-            // do something
-        } else {
+        if (weatherU == 0.0)
+            {
+                // do something
+            } else if (sin(goalBearingRad) == 0.0)
+            {
+                // do something
+            } else {
             resultBearing = acos(weatherV / weatherU)
             resultSpeed = (weatherU + kayakerSpeed * sin(resultBearing)) / sin(goalBearingRad)
-
         }
 
         return resultSpeed
     }
 }
-
-fun main() {
-    /* please sanity check because chat gpt thinks the sin and cos should be the other way round
-    * and i usually trust it*/
-    val speed = 1.0
-    val bearing = 35.0
-    val bearingRad = Math.toRadians(bearing)
-
-    val u = speed * sin(bearingRad)
-    val v = speed * cos(bearingRad)
-
-
-
-    val brng = (Math.toDegrees(atan2(u, v)) + 360) % 360
-
-
-    println("u is $u and v is $v and bearing is $brng")
-}
-
-
-/*
-* so they
-* */
--- backend/src/test/kotlin/com/kayak_backend/services/routes/WeatherKayakTest.kt
+++ backend/src/test/kotlin/com/kayak_backend/services/routes/WeatherKayakTest.kt
@@ -1,12 +1,9 @@
 package com.kayak_backend.services.routes
 
-import com.kayak_backend.services.tides.TideService
-import com.kayak_backend.services.wind.WindService
-import com.kayak_backend.gribReader.GribReader
-import com.kayak_backend.interpolator.Interpolator
 import com.kayak_backend.models.*
 import com.kayak_backend.services.route.WeatherKayak
-import com.kayak_backend.testWindGribConf
+import com.kayak_backend.services.tides.TideService
+import com.kayak_backend.services.wind.WindService
 import io.mockk.every
 import io.mockk.mockk
 import java.time.LocalDateTime
@@ -14,40 +11,62 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class WeatherKayakTest {
+    private val windServiceMock: WindService = mockk<WindService>()
+    private val tideServiceMock: TideService = mockk<TideService>()
+    private val kayak: WeatherKayak = WeatherKayak(windServiceMock, tideServiceMock)
 
-    private val windServiceMock : WindService = mockk<WindService>()
-    private val tideServiceMock : TideService = mockk<TideService>()
-    private val kayak : WeatherKayak = WeatherKayak(windServiceMock, tideServiceMock)
-
+    // may have to change once have factors for wind/tide
     @Test
-    fun weatherOppositeToGoalBearing(){
-        val loc = Location(0.0,0.0)
-        val date = LocalDateTime.of(2024,10,10,10,10)
+    fun weatherOppositeToGoalBearing() {
+        val loc = Location(0.0, 0.0)
+        val date = LocalDateTime.of(2024, 10, 10, 10, 10)
         val bearing = 90.0
 
         every {
             windServiceMock.getWind(loc, date)
-        } returns WindInfo(-1.0,0.0)
+        } returns WindInfo(-1.0, 0.0)
         every {
-            tideServiceMock.getTide(loc,date)
-        } returns TideInfo(0.0,0.0)
+            tideServiceMock.getTide(loc, date)
+        } returns TideInfo(0.0, 0.0)
 
-        println(kayak.getSpeed(date,loc,bearing))
+        println("result is ${kayak.getSpeed(date,loc,bearing)}")
+
+        assertEquals(kayak.getSpeed(date, loc, bearing), 2.0)
     }
 
     @Test
-    fun weatherPerpendicularToGoalBearing(){
-        val loc = Location(0.0,0.0)
-        val date = LocalDateTime.of(2024,10,10,10,10)
+    fun weatherParallelToGoalBearing() {
+        val loc = Location(0.0, 0.0)
+        val date = LocalDateTime.of(2024, 10, 10, 10, 10)
+        val bearing = 90.0
+
+        every {
+            windServiceMock.getWind(loc, date)
+        } returns WindInfo(1.0, 0.0)
+        every {
+            tideServiceMock.getTide(loc, date)
+        } returns TideInfo(0.0, 0.0)
+
+        println("result is ${kayak.getSpeed(date,loc,bearing)}")
+
+        assertEquals(kayak.getSpeed(date, loc, bearing), 4.0)
+    }
+
+    @Test
+    fun weatherPerpendicularToGoalBearing() {
+        val loc = Location(0.0, 0.0)
+        val date = LocalDateTime.of(2024, 10, 10, 10, 10)
         val bearing = 0.0
 
         every {
             windServiceMock.getWind(loc, date)
-        } returns WindInfo(-1.0,0.0)
+        } returns WindInfo(-1.0, 0.0)
         every {
-            tideServiceMock.getTide(loc,date)
-        } returns TideInfo(0.0,0.0)
+            tideServiceMock.getTide(loc, date)
+        } returns TideInfo(0.0, 0.0)
 
-        println(kayak.getSpeed(date,loc,bearing))
+        println(kayak.getSpeed(date, loc, bearing))
+        // what should result be
+        // assertEquals(kayak.getSpeed(date,loc,bearing), )
     }
 }
