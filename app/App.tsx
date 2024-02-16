07427e53946f08152d796fd79979234b997a2599
jlt21 <jlt21@ac.ic.uk>
2024-02-16 14:41:55 +0000

    Remove status bar

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList, Route } from './src/routes';
 import HomeScreen from './src/screens/Home';
@@ -10,7 +9,6 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export const App: React.FC = () => {
   return (
     <NavigationContainer>
-      <StatusBar translucent={true}></StatusBar>
       <Stack.Navigator
         initialRouteName={Route.HOME}
         screenOptions={{
