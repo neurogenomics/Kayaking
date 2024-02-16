1b84b1a67f850e17e326569ad15a29a3e2fa68a5
jlt21 <jlt21@ac.ic.uk>
2024-02-16 13:48:19 +0000

    Add bottom bar

--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -1,8 +1,11 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList, Route } from '../routes';
-import { View, StyleSheet } from 'react-native';
+import React, { useCallback, useMemo, useRef } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 import MapView from 'react-native-maps';
 import { isleOfWight } from '../../constants';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
   container: {
@@ -23,14 +26,34 @@ const styles = StyleSheet.create({
 
 type HomeProps = NativeStackScreenProps<RootStackParamList, Route.HOME>;
 const HomeScreen: React.FC<HomeProps> = () => {
+  // ref
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  // variables
+  const snapPoints = useMemo(() => ['25%', '50%'], []);
+  console.log('handleSheetChanges');
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <MapView
         style={styles.map}
         initialRegion={isleOfWight}
         rotateEnabled={false}
       ></MapView>
-    </View>
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}
+      >
+        <View style={styles.container}>
+          <Text>Awesome 🎉</Text>
+        </View>
+      </BottomSheet>
+    </GestureHandlerRootView>
   );
 };
 
