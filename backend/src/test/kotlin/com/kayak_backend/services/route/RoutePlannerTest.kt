7a32609a08fbf003049fc62879a2eb976d68aad6
3mily8rown <69977813+3mily8rown@users.noreply.github.com>
2024-02-16 21:42:26 +0000

    removed polygonMaker from route planner tests

--- backend/src/test/kotlin/com/kayak_backend/services/route/RoutePlannerTest.kt
+++ backend/src/test/kotlin/com/kayak_backend/services/route/RoutePlannerTest.kt
@@ -12,7 +12,26 @@ import kotlin.test.assertEquals
 
 class RoutePlannerTest {
     private val legTimerMock = mockk<LegTimer>()
-    private val polygonMaker = PolygonMaker()
+    private val polygon: Polygon
+    private val location = Location(0.0, 0.0)
+    private val startTime = LocalDateTime.now()
+    private val duration = 50L
+
+    init {
+        val geometryFactory = GeometryFactory()
+        val linearRing =
+            geometryFactory.createLinearRing(
+                arrayOf(
+                    Coordinate(1.5, 1.5),
+                    Coordinate(1.5, -1.5),
+                    Coordinate(-1.5, -1.5),
+                    Coordinate(-1.5, 1.5),
+                    Coordinate(1.5, 1.5),
+                ),
+            )
+
+        polygon = geometryFactory.createPolygon(linearRing)
+    }
 
     private val startPos: List<StartPos> =
         listOf(
@@ -24,10 +43,6 @@ class RoutePlannerTest {
 
     @Test
     fun generatesRoutesWhichStartAndEndAtValidPoints() {
-        val location = Location(0.0, 0.0)
-        val startTime = LocalDateTime.now()
-        val duration = 50L
-        val polygon = polygonMaker.createPolygon(polygonMaker.polygon1)
         val routePlanner = RoutePlanner(polygon, startPos, 100000000)
 
         every { legTimerMock.getDuration(any(), any()) } returns 4L
@@ -35,7 +50,7 @@ class RoutePlannerTest {
         val result = (
             routePlanner.generateRoutes(
                 { location distanceTo it.location < 1000000000.0 },
-                { legTimerMock.getDuration(it, startTime) < duration * 60 },
+                { legTimerMock.getDuration(it, startTime) < duration },
             ).take(5).toList()
         )
 
@@ -47,10 +62,6 @@ class RoutePlannerTest {
 
     @Test
     fun routesGeneratedWhenOnlyOneStartPoint() {
-        val location = Location(0.0, 0.0)
-        val startTime = LocalDateTime.now()
-        val duration = 50L
-        val polygon = polygonMaker.createPolygon(polygonMaker.polygon1)
         val start = StartPos(Location(1.0, 1.0), "test")
         val routePlanner = RoutePlanner(polygon, listOf(start), 100000000)
 
@@ -59,7 +70,7 @@ class RoutePlannerTest {
         val result = (
             routePlanner.generateRoutes(
                 { location distanceTo it.location < 1000000 },
-                { legTimerMock.getDuration(it, startTime) < duration * 60 },
+                { legTimerMock.getDuration(it, startTime) < duration },
             ).take(5).toList()
         )
 
@@ -69,18 +80,60 @@ class RoutePlannerTest {
         }
     }
 
+    // this test will be redundant (and break) once we add circular routes
     @Test
-    fun returnsEmptySequenceWhenNoRoutesPossibleWithinDuration() {}
+    fun returnsEmptySequenceWhenNoRoutesPossibleWithinDuration() {
+        val routePlanner = RoutePlanner(polygon, startPos, 100000000)
+        val smallDuration = 1L
+
+        every { legTimerMock.getDuration(any(), any()) } returns 4L
+
+        val result = (
+            routePlanner.generateRoutes(
+                { location distanceTo it.location < 1000000000.0 },
+                { legTimerMock.getDuration(it, startTime) < smallDuration },
+            ).take(5).toList()
+        )
+
+        assert(result.isEmpty())
+    }
 
     @Test
-    fun returnsEmptySequenceWhenNoStartingPointsCloseEnough() {}
+    fun returnsEmptySequenceWhenNoStartingPointsCloseEnough() {
+        val routePlanner = RoutePlanner(polygon, startPos, 100000000)
+
+        every { legTimerMock.getDuration(any(), any()) } returns 4L
+
+        val result = (
+            routePlanner.generateRoutes(
+                { location distanceTo it.location < 10 },
+                { legTimerMock.getDuration(it, startTime) < duration },
+            ).take(5).toList()
+        )
+
+        assert(result.isEmpty())
+    }
 
     @Test
-    fun returnsLongestPossibleRoutesOutOfThoseGenerated() {}
+    fun returnsLongestRoutesFirstOutOfThoseGenerated() {
+        val routePlanner = RoutePlanner(polygon, startPos, 100000000)
+
+        every { legTimerMock.getDuration(any(), any()) } returns 4L
+
+        val result = (
+            routePlanner.generateRoutes(
+                { location distanceTo it.location < 1000000000.0 },
+                { legTimerMock.getDuration(it, startTime) < duration },
+            ).take(5).toList()
+        )
+
+        result.zipWithNext { a, b ->
+            assert(a.length >= b.length)
+        }
+    }
 
     @Test
     fun sectionCombinerHasNext() {
-        val polygon = polygonMaker.createPolygon(polygonMaker.polygon1)
         val routePlanner = RoutePlanner(polygon, startPos, 10)
         val sectionCombiner = routePlanner.SectionCombiner()
         assert(sectionCombiner.hasNext())
@@ -89,7 +142,6 @@ class RoutePlannerTest {
     // SectionCombiner tests
     @Test
     fun sectionCombinerGetNext() {
-        val polygon = polygonMaker.createPolygon(polygonMaker.polygon1)
         val routePlanner = RoutePlanner(polygon, startPos, 10000)
         val sectionCombiner = routePlanner.SectionCombiner()
 
@@ -109,33 +161,5 @@ class RoutePlannerTest {
         val actualLeg = sectionCombiner.next()
 
         assertEquals(expectedCombinedLeg, actualLeg)
-    }
-
-    class PolygonMaker() {
-        private val geometryFactory = GeometryFactory()
-
-        val polygon1 =
-            arrayOf(
-                Coordinate(1.5, 1.5),
-                Coordinate(1.5, -1.5),
-                Coordinate(-1.5, -1.5),
-                Coordinate(-1.5, 1.5),
-                Coordinate(1.5, 1.5),
-            )
-
-        val polygon2 =
-            arrayOf(
-                Coordinate(1.0, 1.0),
-                Coordinate(1.5, -1.5),
-                Coordinate(-1.5, -1.5),
-                Coordinate(-1.5, 1.5),
-                Coordinate(1.0, 1.0),
-            )
-
-        fun createPolygon(vertices: Array<Coordinate>): Polygon {
-            val linearRing = geometryFactory.createLinearRing(vertices)
-
-            return geometryFactory.createPolygon(linearRing)
-        }
     }
 }
