4d2d37329cd9340bcd194464309cc155ed51eefa
v4mika <vamikag@gmail.com>
2024-02-16 23:07:15 +0000

    looking better

--- app/components/MapVisualisation.tsx
+++ app/components/MapVisualisation.tsx
@@ -1,6 +1,6 @@
-import MapView, { Marker, Polyline } from 'react-native-maps';
+import MapView, { Polyline, PROVIDER_GOOGLE } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserInput } from '../src/models/userInputModel';
 import { LocationModel } from '../src/models/locationModel';
 import { isleOfWight } from '../constants';
@@ -13,6 +13,13 @@ type MapVisualisationProps = {
   route: {
     params: { user: UserInput };
   };
+};
+
+type Arrow = {
+  left: LocationModel;
+  right: LocationModel;
+  top: LocationModel;
+  bottom: LocationModel;
 };
 
 type ArrowCoords = {
@@ -37,27 +44,31 @@ export const MapVisualisation: React.FC<MapVisualisationProps> = () => {
     lonRes: 0.05,
   };
 
-  // TODO: move these to colours file
-  const windColours: string[] = [
-    'rgb(173, 216, 230)',
-    'rgb(135, 206, 250)',
-    'rgb(0, 191, 255)',
-    'rgb(30, 144, 255)',
-    'rgb(0, 0, 255)',
-  ];
+  // TODO: put this in colours file
+  interface ColorMap {
+    [category: string]: string;
+  }
+
+  const windColorMap: ColorMap = {
+    Calm: 'rgb(173, 216, 230)', // Light Blue
+    'Light Breeze': 'rgb(135, 206, 250)', // Sky Blue
+    'Gentle Breeze': 'rgb(0, 191, 255)', // Deep Sky Blue
+    'Moderate Breeze': 'rgb(30, 144, 255)', // Dodger Blue
+    'Fresh Breeze': 'rgb(0, 0, 255)', // Blue
+  };
 
   // Colours arrows according to the Beaufort scale
   const getColour = (magnitude: number) => {
     if (magnitude <= 1) {
-      return windColours[0];
+      return windColorMap['Calm'];
     } else if (magnitude <= 3) {
-      return windColours[1];
+      return windColorMap['Light Breeze'];
     } else if (magnitude <= 6) {
-      return windColours[2];
+      return windColorMap['Gentle Breeze'];
     } else if (magnitude <= 10) {
-      return windColours[3];
+      return windColorMap['Moderate Breeze'];
     }
-    return windColours[4];
+    return windColorMap['Fresh Breeze'];
   };
 
   const rotateAroundPoint = (
@@ -83,6 +94,53 @@ export const MapVisualisation: React.FC<MapVisualisationProps> = () => {
     return rotatedModel;
   };
 
+  function getArrow(
+    left: math.Matrix,
+    right: math.Matrix,
+    top: math.Matrix,
+    origin: math.Matrix,
+    thetaRad: number,
+  ): Arrow {
+    // Rotating arrow according to angle
+    const leftModel = rotateAroundPoint(left, origin, thetaRad);
+    const rightModel = rotateAroundPoint(right, origin, thetaRad);
+    const topModel = rotateAroundPoint(top, origin, thetaRad);
+
+    const slope: number =
+      (rightModel.latitude - leftModel.latitude) /
+      (rightModel.longitude - leftModel.longitude);
+    const perpSlope: number = -1 / slope;
+
+    const perpEquation = (longitude) =>
+      perpSlope * (longitude - topModel.longitude) + topModel.latitude;
+
+    const intersectLon =
+      (1 / (slope - perpSlope)) *
+      (slope * leftModel.longitude -
+        leftModel.latitude -
+        perpSlope * topModel.longitude +
+        topModel.latitude);
+    const intersectLat = perpEquation(intersectLon);
+
+    // Create a matrix for the intersection point
+    const intersection = math.matrix([intersectLat, intersectLon]);
+
+    const topMat = math.matrix([topModel.latitude, topModel.longitude]);
+
+    const bottomModel: LocationModel = rotateAroundPoint(
+      topMat,
+      intersection,
+      Math.PI,
+    );
+
+    return {
+      left: leftModel,
+      right: rightModel,
+      top: topModel,
+      bottom: bottomModel,
+    };
+  }
+
   const makeArrowCoordinates = (grid: GridModel, gridRes: ResolutionModel) => {
     const markers: ArrowCoords[] = [];
     for (let i = 0; i < grid.latIndex.length; i++) {
@@ -97,12 +155,6 @@ export const MapVisualisation: React.FC<MapVisualisationProps> = () => {
           latitude + gridRes.latRes / 9,
           longitude + gridRes.lonRes / 6,
         ]);
-        const bottom = math.matrix([
-          latitude - gridRes.latRes / 9,
-          longitude + gridRes.lonRes / 6,
-        ]);
-
-        const coordinates = [left, right, top, right, bottom];
 
         // Bearing angle that the arrow needs to be rotated by
         const theta = Math.atan2(grid.grid[i][j].v, grid.grid[i][j].u);
@@ -115,13 +167,16 @@ export const MapVisualisation: React.FC<MapVisualisationProps> = () => {
           grid.grid[i][j].u ** 2 + grid.grid[i][j].v ** 2,
         );
 
-        const rotatedCoords: LocationModel[] = [];
-        for (let i = 0; i < coordinates.length; i++) {
-          rotatedCoords.push(rotateAroundPoint(coordinates[i], origin, theta));
-        }
+        const arrow: Arrow = getArrow(left, right, top, origin, theta);
 
         const arrowPoints: ArrowCoords = {
-          coords: rotatedCoords,
+          coords: [
+            arrow.left,
+            arrow.right,
+            arrow.top,
+            arrow.right,
+            arrow.bottom,
+          ],
           magnitude: magnitude,
         };
         markers.push(arrowPoints);
@@ -150,13 +205,18 @@ export const MapVisualisation: React.FC<MapVisualisationProps> = () => {
 
   return (
     <View style={styles.mapContainer}>
-      <MapView style={styles.map} initialRegion={isleOfWight}>
+      <MapView
+        style={styles.map}
+        initialRegion={isleOfWight}
+        provider={PROVIDER_GOOGLE}
+        rotateEnabled={true}
+      >
         {coords ? (
           coords.map((coord: ArrowCoords, index) => (
             <View key={index}>
               <Polyline
                 coordinates={coord.coords}
-                strokeWidth={1.5}
+                strokeWidth={2}
                 strokeColor={getColour(coord.magnitude)}
               ></Polyline>
             </View>
