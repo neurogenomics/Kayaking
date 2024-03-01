04b527cb0051104c51850f23dce4c7f9ad259185
v4mika <vamikag@gmail.com>
2024-03-01 16:17:12 +0000

    started adjusting length

--- app/src/components/MapVisualisations/WeatherVisualisation.tsx
+++ app/src/components/MapVisualisations/WeatherVisualisation.tsx
@@ -3,12 +3,17 @@ import React, { useEffect, useState } from 'react';
 import { LocationModel } from '../../models/locationModel';
 import { GridModel, GridType, ResolutionModel } from '../../models/gridModel';
 import { getGrid } from '../../services/gridService';
-import { Matrix, Vector } from 'ts-matrix';
 import { tideColorMap, windColorMap } from '../../colors';
 
 type WeatherVisualisationProps = {
   display: GridType;
   date: Date;
+};
+
+type WeatherVector = {
+  location: LocationModel;
+  direction: number;
+  magnitude: number;
 };
 
 type Arrow = {
@@ -44,81 +49,34 @@ export const WeatherVisualisation: React.FC<WeatherVisualisationProps> = ({
     lonRes: 0.05,
   };
 
-  const rotateAroundPoint = (
-    loc: Vector,
-    origin: Vector,
-    thetaRad: number,
-  ): LocationModel => {
-    const diff = loc.subtract(origin);
-    const diffMat = new Matrix(2, 1, [[diff.values[0]], [diff.values[1]]]);
-
-    // Clockwise rotation matrix
-    const rotationMat = new Matrix(2, 2, [
-      [Math.cos(thetaRad), Math.sin(thetaRad)],
-      [-Math.sin(thetaRad), Math.cos(thetaRad)],
-    ]);
-    const flattenMul = rotationMat
-      .multiply(diffMat)
-      .values.reduce((accumulator, value) => accumulator.concat(value));
-    const rotated: Vector = new Vector(flattenMul);
-    const final: Vector = rotated.add(origin);
-
-    return {
-      latitude: final.values[0],
-      longitude: final.values[1],
-    };
+  const getArrow = (
+    vectors: WeatherVector[],
+    gridRes: ResolutionModel,
+    minMag: number,
+    maxMag: number,
+  ) => {
+    const arrows: ArrowCoords[] = [];
+    const maxLength: number = Math.min(gridRes.latRes, gridRes.lonRes);
+    for (let i = 0; i < vectors.length; i++) {
+      const vec = vectors[i];
+      const x = (maxLength * (vec.magnitude - minMag)) / (maxMag - minMag);
+      const endArrow: LocationModel = {
+        longitude: vec.location.longitude + x * Math.sin(vec.direction),
+        latitude: vec.location.latitude + x * Math.cos(vec.direction),
+      };
+      const coords = {
+        coords: [vec.location, endArrow],
+        magnitude: vec.magnitude,
+      };
+      arrows.push(coords);
+    }
+    setCoords(arrows);
   };
 
-  function getArrow(
-    left: Vector,
-    right: Vector,
-    top: Vector,
-    origin: Vector,
-    thetaRad: number,
-  ): Arrow {
-    // Rotating arrow according to angle
-    const leftModel = rotateAroundPoint(left, origin, thetaRad);
-    const rightModel = rotateAroundPoint(right, origin, thetaRad);
-    const topModel = rotateAroundPoint(top, origin, thetaRad);
-
-    // Maths to reflect the top vertex of the arrow along the arrow line to get the bottom vertex
-    const slope: number =
-      (rightModel.latitude - leftModel.latitude) /
-      (rightModel.longitude - leftModel.longitude);
-    const perpSlope: number = -1 / slope;
-
-    const perpEquation = (longitude: number) =>
-      perpSlope * (longitude - topModel.longitude) + topModel.latitude;
-
-    const intersectLon =
-      (1 / (slope - perpSlope)) *
-      (slope * leftModel.longitude -
-        leftModel.latitude -
-        perpSlope * topModel.longitude +
-        topModel.latitude);
-    const intersectLat = perpEquation(intersectLon);
-
-    // Create a matrix for the intersection point
-    const intersection = new Vector([intersectLat, intersectLon]);
-    const topMat = new Vector([topModel.latitude, topModel.longitude]);
-
-    // Rotating top point 180 degrees around the intersection point to get the new point
-    const bottomModel: LocationModel = rotateAroundPoint(
-      topMat,
-      intersection,
-      Math.PI,
-    );
-
-    return {
-      left: leftModel,
-      right: rightModel,
-      top: topModel,
-      bottom: bottomModel,
-    };
-  }
-
-  const makeArrowCoordinates = (grid: GridModel, gridRes: ResolutionModel) => {
-    const markers: ArrowCoords[] = [];
+  const makeArrowCoordinates = (grid: GridModel) => {
+    const vectors: WeatherVector[] = [];
+    let minMag: number = Infinity;
+    let maxMag: number = 0;
     for (let i = 0; i < grid.latIndex.length; i++) {
       for (let j = 0; j < grid.lonIndex.length; j++) {
         if (grid.grid[i][j] && grid.latIndex[i] && grid.lonIndex[j]) {
@@ -127,43 +85,28 @@ export const WeatherVisualisation: React.FC<WeatherVisualisationProps> = ({
 
           // Coordinates of right facing arrow
           // Constants chosen to prevent arrow from filling entire grid
-          const left = new Vector([latitude, longitude - gridRes.lonRes / 3]);
-          const right = new Vector([latitude, longitude + gridRes.lonRes / 3]);
-          const top = new Vector([
-            latitude + gridRes.latRes / 9,
-            longitude + gridRes.lonRes / 6,
-          ]);
 
           // Bearing angle that the arrow needs to be rotated by
           const theta = Math.atan2(grid.grid[i][j].v, grid.grid[i][j].u);
 
-          // Origin around which arrow is rotated
-          const origin = new Vector([latitude, longitude]);
-
           // Magnitude of wind/tide vector
-          const magnitude: number = Math.sqrt(
+          const magnitude = Math.sqrt(
             grid.grid[i][j].u ** 2 + grid.grid[i][j].v ** 2,
           );
 
-          const arrow = getArrow(left, right, top, origin, theta);
+          minMag = Math.min(minMag, magnitude);
+          maxMag = Math.max(maxMag, magnitude);
 
-          if (!isNaN(arrow.bottom.latitude) && !isNaN(arrow.bottom.longitude)) {
-            const arrowPoints: ArrowCoords = {
-              coords: [
-                arrow.left,
-                arrow.right,
-                arrow.top,
-                arrow.right,
-                arrow.bottom,
-              ],
-              magnitude: magnitude,
-            };
-            markers.push(arrowPoints);
-          }
+          const vec: WeatherVector = {
+            location: { latitude: latitude, longitude: longitude },
+            direction: theta,
+            magnitude: magnitude,
+          };
+          vectors.push(vec);
         }
       }
     }
-    setCoords(markers);
+    getArrow(vectors, gridResolution, minMag, maxMag);
   };
 
   const getArrowGrid = async (date: Date) => {
@@ -176,7 +119,7 @@ export const WeatherVisualisation: React.FC<WeatherVisualisationProps> = ({
         date,
       );
       console.log(grid);
-      makeArrowCoordinates(grid, gridResolution);
+      makeArrowCoordinates(grid);
     } catch (error) {
       console.log('Error getting grid: ', error);
     }
