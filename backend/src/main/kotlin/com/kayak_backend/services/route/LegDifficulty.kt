477d612618e159a1fe447fc2606962c7da339137
3mily8rown <69977813+3mily8rown@users.noreply.github.com>
2024-03-04 16:43:46 +0000

    calculate difficulty function for leg

--- backend/src/main/kotlin/com/kayak_backend/services/route/LegDifficulty.kt
+++ backend/src/main/kotlin/com/kayak_backend/services/route/LegDifficulty.kt
@@ -3,28 +3,29 @@ package com.kayak_backend.services.route
 import com.kayak_backend.getConf
 import com.kayak_backend.getWaveService
 import com.kayak_backend.getWindService
+import com.kayak_backend.models.Location
 import com.kayak_backend.services.waves.WaveService
 import com.kayak_backend.services.wind.WindService
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
-class LegDifficulty {
+// should this be called routeDifficulty?
+class LegDifficulty(
+    windService: WindService = getWindService(getConf("./config.yaml")),
+    waveService: WaveService = getWaveService(getConf("./config.yaml")),
+) {
     // maps leg to (time, difficulty)
     private val difficultyCache = mutableMapOf<Leg, MutableMap<Long, Int>>()
 
     fun getDifficulty(
         route: Route,
-        windService: WindService = getWindService(getConf("./config.yaml")),
-        waveService: WaveService = getWaveService(getConf("./config.yaml")),
         dateTime: LocalDateTime,
     ): Int {
-        return getLegDifficulty(route.locations, windService, waveService, dateTime)
+        return getLegDifficulty(route.locations, dateTime)
     }
 
     private fun getLegDifficulty(
         leg: Leg,
-        windService: WindService,
-        waveService: WaveService,
         dateTime: LocalDateTime,
     ): Int {
         val epoch = dateTime.toEpochSecond(ZoneOffset.UTC)
@@ -36,10 +37,25 @@ class LegDifficulty {
         leg: Leg,
         dateTime: LocalDateTime,
     ): Int {
-        if (leg is Leg.SingleLeg) {
-            return 0
-        } else {
-            return 1
+        return when (leg) {
+            is Leg.SingleLeg -> {
+                val midpoint =
+                    Location(
+                        (leg.start.latitude + leg.start.latitude) / 2,
+                        (leg.start.longitude + leg.start.longitude) / 2,
+                    )
+                classifyConditions(dateTime, midpoint)
+            }
+            is Leg.MultipleLegs -> {
+                leg.legs.maxOf { getLegDifficulty(leg, dateTime) }
+            }
         }
+    }
+
+    private fun classifyConditions(
+        dateTime: LocalDateTime,
+        location: Location,
+    ): Int  {
+        return 0
     }
 }
