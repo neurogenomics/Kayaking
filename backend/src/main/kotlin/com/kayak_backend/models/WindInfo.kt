117e40c261d4633166e3ab471fc300af751c8837
3mily8rown <69977813+3mily8rown@users.noreply.github.com>
2024-03-04 18:00:49 +0000

    added beaufort scale values to leg difficulty

--- backend/src/main/kotlin/com/kayak_backend/models/WindInfo.kt
+++ backend/src/main/kotlin/com/kayak_backend/models/WindInfo.kt
@@ -5,5 +5,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class WindInfo(val u: Double, val v: Double)
 
+fun getWindMagnitude(windInfo: WindInfo): Double {
+    return windInfo.u * windInfo.u * windInfo.v * windInfo.v
+}
+
 @Serializable
 data class WindGrid(val grid: List<List<WindInfo?>>, val latIndex: List<Double>, val lonIndex: List<Double>)
--- backend/src/main/kotlin/com/kayak_backend/services/route/LegDifficulty.kt
+++ backend/src/main/kotlin/com/kayak_backend/services/route/LegDifficulty.kt
@@ -4,18 +4,24 @@ import com.kayak_backend.getConf
 import com.kayak_backend.getWaveService
 import com.kayak_backend.getWindService
 import com.kayak_backend.models.Location
+import com.kayak_backend.models.getWindMagnitude
 import com.kayak_backend.services.waves.WaveService
 import com.kayak_backend.services.wind.WindService
 import java.time.LocalDateTime
 import java.time.ZoneOffset
+import kotlin.math.max
 
 // should this be called routeDifficulty?
 class LegDifficulty(
-    windService: WindService = getWindService(getConf("./config.yaml")),
-    waveService: WaveService = getWaveService(getConf("./config.yaml")),
+    private val windService: WindService = getWindService(getConf("./config.yaml")),
+    private val waveService: WaveService = getWaveService(getConf("./config.yaml")),
 ) {
     // maps leg to (time, difficulty)
     private val difficultyCache = mutableMapOf<Leg, MutableMap<Long, Int>>()
+
+    // source: https://www.metoffice.gov.uk/weather/guides/coast-and-sea/beaufort-scale
+    private val waveLevels = listOf(0.0, 0.1, 0.3, 1.0, 1.5, 2.5, 4.0, 5.5, 7.5, 10.0, 12.5, 16.0)
+    private val windLevels = listOf(1.0, 2.0, 3.0, 4.0, 5.0, 8.0, 11.0, 14.0, 17.0, 21.0, 24.0, 28.0, 32.0)
 
     fun getDifficulty(
         route: Route,
@@ -55,7 +61,13 @@ class LegDifficulty(
     private fun classifyConditions(
         dateTime: LocalDateTime,
         location: Location,
-    ): Int  {
-        return 0
+    ): Int {
+        val windInfo = windService.getWind(location, dateTime)
+        val waveInfo = waveService.getWave(location, dateTime)
+
+        val windLevel = windLevels.indexOfFirst { it >= (getWindMagnitude(windInfo)) }
+        val waveLevel = waveLevels.indexOfFirst { it >= (waveInfo.height) }
+
+        return if (windLevel == -1 || waveLevel == -1) 12 else max(windLevel, waveLevel)
     }
 }
