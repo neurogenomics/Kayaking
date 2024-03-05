a1a120ddc45ed03f1d4528a0a0c96311d76e4710
jlt21 <jlt21@ac.ic.uk>
2024-03-05 17:28:21 +0000

    Plan routes around coastline

--- backend/src/main/kotlin/com/kayak_backend/routes/RoutePlannerRoute.kt
+++ backend/src/main/kotlin/com/kayak_backend/routes/RoutePlannerRoute.kt
@@ -1,6 +1,7 @@
 package com.kayak_backend.routes
 
 import com.kayak_backend.models.Location
+import com.kayak_backend.services.coastline.IsleOfWightCoastline
 import com.kayak_backend.services.route.LegTimer
 import com.kayak_backend.services.route.RoutePlanner
 import com.kayak_backend.services.route.TimedRoute
@@ -9,11 +10,14 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.*
 import io.ktor.server.util.getOrFail
 
+val coastline = IsleOfWightCoastline()
+
 fun Route.planRoute(
     routePlanner: RoutePlanner,
     legTimer: LegTimer,
     startPositionFilterDistance: Double = 1000.0,
 ) {
+    val coastlineLocations = routePlanner.getBaseRoute().coordinates.map { Location(it.x, it.y) }
     route("/planRoute") {
         get {
             val lat = call.parameters.getOrFail<Double>("lat")
@@ -21,9 +25,12 @@ fun Route.planRoute(
             val duration = call.parameters.getOrFail<Double>("duration")
             val startTime = getDateParameter(call.parameters, "startDateTime")
             val location = Location(lat, lng)
+
+            val coastlineLocation = coastlineLocations.minBy { it distanceTo location }
+
             val routes =
                 routePlanner.generateRoutes(
-                    { location distanceTo it.location < startPositionFilterDistance },
+                    { coastlineLocation distanceTo it.location < startPositionFilterDistance },
                     { legTimer.getDuration(it, startTime) < duration * 60 },
                 ).take(20).toList()
 
