8de8891c9d2eed7d558c022be4396eb7aca3b367
3mily8rown <69977813+3mily8rown@users.noreply.github.com>
2024-03-08 02:34:58 +0000

    changed call parameters to String types in dangerous wind route

--- backend/src/main/kotlin/com/kayak_backend/routes/DangerousWindRoute.kt
+++ backend/src/main/kotlin/com/kayak_backend/routes/DangerousWindRoute.kt
@@ -6,13 +6,26 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.util.*
+import org.json.JSONArray
 
 fun Route.dangerousWind(dangerousWind: DangerousWindService) {
     route("/dangerouswind") {
         get {
-            val locations = call.parameters.getOrFail<List<Location>>("locations")
-            val checkpoints = call.parameters.getOrFail<List<Long>>("checkpoints")
             val startTime = getDateParameter(call.parameters, "startDateTime")
+
+            val latsArray = JSONArray(call.parameters.getOrFail<String>("lats"))
+            val longsArray = JSONArray(call.parameters.getOrFail<String>("longs"))
+            val checkpointArray = JSONArray(call.parameters.getOrFail<String>("checkpoints"))
+
+            val checkpoints = Array(checkpointArray.length()) { checkpointArray.getLong(it) }.toList()
+            val lats = Array(latsArray.length()) { latsArray.getDouble(it) }.toList()
+            val longs = Array(longsArray.length()) { longsArray.getDouble(it) }.toList()
+
+            val locations =
+                lats.zip(longs).map { (lat, long) ->
+                    Location(lat, long)
+                }
+
             call.respond(dangerousWind.findBadWinds(locations, checkpoints, startTime))
         }
     }
