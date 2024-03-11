f1d309df233f7e236afbd06fa2fd157009c0d767
jlt21 <jlt21@ac.ic.uk>
2024-03-11 14:20:30 +0000

    Test tide times

---
+++ app/tests/src/services/tideTimesService.test.ts
@@ -0,0 +1,57 @@
+import { LocationModel } from '../../../src/models/locationModel';
+import { TideEvent, TideInfo } from '../../../src/models/tideModel';
+import { getTideTimes } from '../../../src/services/tideTimesService';
+import * as utils from '../../../src/services/utils';
+
+const getDataMock = jest.fn();
+
+beforeAll(() => {
+  jest.spyOn(utils, 'getData').mockImplementation(getDataMock);
+});
+
+describe('getTideTimes', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should call getData with the correct URL', async () => {
+    const location: LocationModel = {
+      latitude: 40.7128,
+      longitude: -74.0064,
+    };
+    await getTideTimes(location);
+    const expectedUrl = `tidetimes?lat=${location.latitude}&lon=${location.longitude}`;
+    expect(getDataMock).toHaveBeenCalledWith(expectedUrl);
+  });
+
+  it('should return the tide events', async () => {
+    const location: LocationModel = {
+      latitude: 40.7128,
+      longitude: -74.0064,
+    };
+    const mockTideEvents: TideEvent[] = [
+      {
+        source: {
+          id: '1',
+          name: 'Tide Station 1',
+          location: location,
+        },
+        datetime: new Date('2024-01-01T12:00:00'),
+        height: 3.5,
+      },
+      {
+        source: {
+          id: '2',
+          name: 'Tide Station 2',
+          location: location,
+        },
+        datetime: new Date('2024-01-01T18:00:00'),
+        height: 1.2,
+      },
+    ];
+    getDataMock.mockResolvedValue(mockTideEvents);
+
+    const result = await getTideTimes(location);
+    expect(result).toEqual(mockTideEvents);
+  });
+});
